dcf9140b28a1e4b477412f19fb9354bba85dae63
wabyroade8918 <wabyroade8918@H339R6S1.STUDENT.ad.fgcu.edu>
2017-09-28 10:19:55 -0400

    First meaningful commit to Integration Project.

--- WAB-IntegrationProject-COP2006/src/integrationPkg/Integration.java
+++ WAB-IntegrationProject-COP2006/src/integrationPkg/Integration.java
@@ -1,11 +1,36 @@
 package integrationPkg;
 
+import java.util.Scanner;
+
+//Wyatt Byroade
+//Integration Project - COP 2006
+//Fall 2017
+
 public class Integration {
+	public static Scanner scan;
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubtest
-		System.out.println("Eclipse kinda sucks.");
+		System.out.println("Welcome to my Integration Project.");
 
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Would you like to encode or decode text?:\n1-encode\n2-decode");
+		int encodeDecodeChoice = scan.nextInt();
+		
+		if (encodeDecodeChoice == 1) {
+			System.out.println("You selected to encode text.\nEnter text to encode:");
+			String encodeInput = scan.nextLine();
+		}
+		else if (encodeDecodeChoice == 2) {
+			System.out.println("You selected to decode text.\nEnter text to encode:");
+			String decodeInput = scan.nextLine();
+		}
+		
+		
+		System.out.println("Program Completed Successfully.");
+		scan.close();
 	}
 
 }
---
+++ WAB-IntegrationProject-COP2006/src/integrationPkg/WeakCrypt.java
@@ -0,0 +1,10 @@
+package integrationPkg;
+
+public class WeakCrypt {
+	
+	public static String encodeText(String inputText) {
+		final String textEncoded = inputText;
+		return textEncoded;
+	}
+
+}
