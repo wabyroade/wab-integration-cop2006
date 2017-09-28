7da08962848fc2c68b2ddeeee4422420cb3af208
wabyroade8918 <wabyroade8918@H339R6S1.STUDENT.ad.fgcu.edu>
2017-09-28 09:16:15 -0400

    test commit to local git repository  -09/28/17 9:16AM

---
+++ WAB-IntegrationProject-COP2006/src/integrationPkg/Integration.java
@@ -0,0 +1,11 @@
+package integrationPkg;
+
+public class Integration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubtest
+		System.out.println("Eclipse kinda sucks.");
+
+	}
+
+}
