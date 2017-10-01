30e8bf56db45b3cda4e985b7162822e18ecd37f5
Wyatt <Wyatt@10.0.0.245>
2017-09-30 23:05:00 -0400

    Commit of Integration Project just before SP1

--- WAB-IntegrationProject-COP2006/src/integrationPkg/Integration.java
+++ WAB-IntegrationProject-COP2006/src/integrationPkg/Integration.java
@@ -5,32 +5,60 @@ import java.util.Scanner;
 //Wyatt Byroade
 //Integration Project - COP 2006
 //Fall 2017
+//SP1 - September 30, 2017
+
+
+//TODO: Work on more complex cipher method, reduce duplicate code
 
 public class Integration {
 	public static Scanner scan;
-	
+// declaring variables of Java built-in(primitive) data types
+// byte, short, int, long, float, double, char and boolean
+// also I shoehorned in some integer division in next two lines. My idea was to
+// create some kind of basic cipher that would use division and multiplication
+// but I couldn't get it working, so I'm using a basic offset substitution that
+// only uses addition and modulus
+	static int encodeOffset = 100 / 10;
+	static int decodeOffset = 16 / 10;
+	byte unusedByte = 0;
+	short unusedShort = 0;
+	long unusedLong = 0;
+	float unusedFloat = 0.1f;
+	double unusedDouble = 0.1;
+	char unusedChar = 'A';
+	boolean unusedBoolean = false;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubtest
-		System.out.println("Welcome to my Integration Project.");
-
+// instantiate a new Scanner object as well as a new WeakCrypt object
 		Scanner scan = new Scanner(System.in);
+		WeakCrypt thisCryptObj = new WeakCrypt();
 
-		System.out.println("Would you like to encode or decode text?:\n1-encode\n2-decode");
+// display welcome message and initial prompt, then receive user input
+		System.out.println(
+				"Welcome to my integration project.\nWould you like to encode" +
+				"or decode text?:\n1-encode\n2-decode");
 		int encodeDecodeChoice = scan.nextInt();
-		
-		if (encodeDecodeChoice == 1) {
-			System.out.println("You selected to encode text.\nEnter text to encode:");
-			String encodeInput = scan.nextLine();
-		}
-		else if (encodeDecodeChoice == 2) {
-			System.out.println("You selected to decode text.\nEnter text to encode:");
-			String decodeInput = scan.nextLine();
-		}
-		
-		
-		System.out.println("Program Completed Successfully. Modified.");
-		scan.close();
-	}
+		scan.nextLine();
 
+// if/else logical blocks here
+		if (encodeDecodeChoice == 1) {
+			System.out.println(
+					"You selected to encode text.\nEnter text to encode:");
+			String encodeInput = scan.nextLine().toUpperCase();
+// call encodeText() method with arguments encodeInput and encodeOffset
+			thisCryptObj.encodeText(encodeInput, encodeOffset);
+			System.out.println(thisCryptObj.getEncodedText());
+		} else if (encodeDecodeChoice == 2) {
+			System.out.println(
+					"You selected to decode text.\nEnter text to encode:");
+			String decodeInput = scan.nextLine().toUpperCase();
+// call decodeText() method with arguments decodeInput and decodeOffset
+			thisCryptObj.decodeText(decodeInput, decodeOffset);
+			System.out.println(thisCryptObj.getDecodedText());
+		} else {
+			System.out.println("You have selected an invalid option. Exiting.");
+		}
+		scan.close();
+
+	}
 }
--- WAB-IntegrationProject-COP2006/src/integrationPkg/WeakCrypt.java
+++ WAB-IntegrationProject-COP2006/src/integrationPkg/WeakCrypt.java
@@ -1,10 +1,65 @@
 package integrationPkg;
 
+//Wyatt Byroade
+//Integration Project - COP 2006
+//Fall 2017
+//SP1 - September 30, 2017
+
 public class WeakCrypt {
 	
-	public static String encodeText(String inputText) {
-		final String textEncoded = inputText;
-		return textEncoded;
+// private fields declared here
+	private String textEncoded = new String();
+	private String textDecoded = new String();
+	private String alphabetString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// method declaration with multiple parameters - technically this serves
+// as setter for the private field textEncoded
+	public void encodeText(String inputText, int offset) {
+		char tempArray[] = new char[inputText.length()];
+		for (int i = 0; i < inputText.length(); i++) {
+			char thisChar = inputText.charAt(i);
+			int alphaIndex = alphabetString.indexOf(thisChar);
+// logical blocks within method
+			if(alphaIndex > 0) {
+// mathematical operations
+				alphaIndex = (alphaIndex + offset)%26;
+				tempArray[i] = alphabetString.charAt(alphaIndex);
+			}
+			else {
+				tempArray[i] = thisChar;
+			}
+		}
+		textEncoded = new String(tempArray);
+	}
+	
+// method declaration with multiple parameters - technically this serves
+// as setter for the private field textDecoded
+	public void decodeText(String inputText, int offset) {
+		char tempArray[] = new char[inputText.length()];
+		for (int i = 0; i < inputText.length(); i++) {
+			char thisChar = inputText.charAt(i);
+			int alphaIndex = alphabetString.indexOf(thisChar);
+// logical blocks within method
+			if(alphaIndex > 0) {
+// mathematical operations
+				alphaIndex = (alphaIndex + offset)%26;
+				tempArray[i] = alphabetString.charAt(alphaIndex);
+			}
+			else {
+				tempArray[i] = thisChar;
+			}
+		}
+		textDecoded = new String(tempArray);
 	}
 
+// public getter method for the private encodedText field
+	public String getEncodedText() {
+		return textEncoded;
+	}
+	
+// public getter method for the private decodedText field
+	public String getDecodedText() {
+		return textDecoded;
+	}
+	
 }
